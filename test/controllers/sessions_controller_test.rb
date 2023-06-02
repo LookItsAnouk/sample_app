fcf1da528f8162c087c8304b0c9dca093e63d6cd
Anouk Borris <anoukborris@gmail.com>
2023-06-02 23:19:37 +0000

    end of day

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,4 +5,9 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     get login_path
     assert_response :success
   end
+  
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url, status: :see_other
+  end
 end
