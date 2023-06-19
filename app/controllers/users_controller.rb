699d069a98266418656fda64e7e0475da0f872b5
Anouk Borris <anoukborris@gmail.com>
2023-06-19 22:47:32 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: 
+  before_action :admin_user,     only: :destroy
+  
   def show
     @user = User.find(params[:id])
   end
@@ -10,18 +14,38 @@ class UsersController < ApplicationController
   def create
   @user = User.new(user_params)  # Not the final implementation!
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-      # Handle a successful save.
+       @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
     end
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url, status: :see_other
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_url and return unless FILL_IN
+  end
+
   
 private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
   end
 end
